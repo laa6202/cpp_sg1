e76052e130c2e435784df3299b8de1c453f54b73
安 卢 <laa6203@163.com>
2017-05-22 20:28:11 +0800

    update t8

---
+++ src/alg2.cpp
@@ -0,0 +1,27 @@
+/*
+ * alg2.cpp
+ *
+ *  Created on: 2017Äê5ÔÂ22ÈÕ
+ *      Author: Administrator
+ */
+
+#include "alg2.h"
+#include "imgCheck.h"
+#include "tools.h"
+
+
+int getBackFn2(String & fn0){
+	//fn0 = "t8/Mon May 22 16-37-00.bmp";
+	//fn0 = "t8/Mon May 22 16-37-01.bmp";
+	fn0 = "t8/Mon May 22 16-41-07.bmp";
+	cout << "...get back file:" << fn0 <<endl;
+	return 0;
+}
+
+int getFontFn2(String & fn0){
+	//fn0 = "t8/Mon May 22 16-40-33.bmp";
+	fn0 = "t8/Mon May 22 16-42-01.bmp";
+	cout << "...get font file:" << fn0 <<endl;
+	return 0;
+}
+
---
+++ src/alg2.h
@@ -0,0 +1,25 @@
+/*
+ * alg2.h
+ *
+ *  Created on: 2017Äê5ÔÂ22ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef ALG2_H_
+#define ALG2_H_
+
+
+
+#include <iostream>
+#include <string>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+
+int getBackFn2(String & fn0);
+int getFontFn2(String & fn0);
+
+
+#endif /* ALG2_H_ */
